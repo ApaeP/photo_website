af716241191136d224cd93c194712b7f8be2c345
Paul Portier <paulportier@gmail.com>
2021-06-22 21:00:02 +0200

    added swiper

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,29 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import Swiper, { Navigation, Pagination } from 'swiper'
+Swiper.use([Navigation, Pagination]);
+import 'swiper/swiper-bundle.css'
+
 require("stylesheets/application.scss")
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener('turbolinks:load', () => {
+  const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
+
+})
 
