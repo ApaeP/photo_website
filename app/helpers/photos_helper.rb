30ef6223d469d593d769e0da1f22f679ab156f87
Paul Portier <paulportier@gmail.com>
2021-06-28 01:02:54 +0200

    delete useless string

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,6 +1,5 @@
 module PhotosHelper
   def custom_cl_image_tag(key, **kwargs)
-    "key: #{key}\nkwargs: #{kwargs}"
     kwargs[:class].concat(" #{kwargs.delete(:cover) ? 'object-cover' : 'object-contain'}")
 
     cl_image_tag(key, kwargs)
