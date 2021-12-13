f1ef84fbca8a67a41edc96c16af9e57580b9ddf7
Paul Portier <paulportier@gmail.com>
2021-12-13 11:59:36 +0100

    Allow nav with buttons

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -1,11 +1,33 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = []
+  static targets = [ 'right', 'left' ]
 
-  connect() {}
+  connect() {
+    this.initScrollWithButtons()
+  }
 
   scroll(e) {
     this.element.scrollLeft += e.deltaY;
+  }
+
+  initScrollWithButtons() {
+    var rightId, leftId
+    this.rightTarget.addEventListener('mousedown', (e) => { rightId = setInterval(this.scrollRight.bind(this), 1) })
+    this.rightTarget.addEventListener('mouseup',   (e) => { clearInterval(rightId) })
+    this.rightTarget.addEventListener('mouseleave',(e) => { clearInterval(rightId) })
+    this.leftTarget.addEventListener( 'mousedown', (e) => { leftId = setInterval(this.scrollLeft.bind(this), 1) })
+    this.leftTarget.addEventListener( 'mouseup',   (e) => { clearInterval(leftId) })
+    this.leftTarget.addEventListener( 'mouseleave',(e) => { clearInterval(leftId) })
+  }
+
+  scrollRight() {
+    console.log('scroll to the right')
+    this.element.scrollLeft += 4
+  }
+
+  scrollLeft() {
+    console.log('scroll to the left')
+    this.element.scrollLeft -= 4
   }
 }
