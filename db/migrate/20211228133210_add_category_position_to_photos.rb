111ccd38c3d3f7b800c305ab332578e76ceab461
Paul Portier <paulportier@gmail.com>
2021-12-28 16:57:00 +0100

    fix_migration

--- db/migrate/20211228133210_add_category_position_to_photos.rb
+++ db/migrate/20211228133210_add_category_position_to_photos.rb
@@ -1,5 +1,8 @@
 class AddCategoryPositionToPhotos < ActiveRecord::Migration[6.1]
   def change
     add_column :photos, :position, :integer
+    Photo.order(:updated_at).each.with_index(1) do |photo, index|
+      photo.update_column :position, index
+    end
   end
 end
