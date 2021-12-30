c7f7a0e2d44f409a484e662dfc1e85d8b26653c9
Paul Portier <paulportier@gmail.com>
2021-12-30 01:44:23 +0100

    lilfixes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,10 @@
 class Contact < ApplicationRecord
-  validates :content, presence: true, length: { minimum: 15 }
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-
+  validates :content, presence: true, length: { minimum: 15, message: 'Un peu court' }
+  validates_presence_of :email, message: 'Requis'
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP, message: 'Email invalide'
   after_create :send_emails
+
+  private
 
   def send_emails
     ContactToOwnerMailer.with(contact: self).new_contact.deliver_later
